347271433cc433229d4e1ee86cf6053f3a9b6e0f
profsain <husseinimudiking@gmail.com>
2023-07-04 12:41:09 +0100

    add form submission logic

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
 
 const AddPost = () => {
-  const handleSubmit = (e) => { 
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    image: "",
+    tags: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      // check if the name is tags, if so, split the string into an array
+      [name]: name === "tags" ? value.split(",") : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const post = {
-      title: e.target.title.value,
-      content: e.target.content.value,
-      image: e.target.image.value,
-      tags: e.target.tags.value.split(','),
-    };
-    console.log(post);
+    console.log(formData);
+    // submit form data to database
+    fetch("http://localhost:4040/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        // clear form
+        setFormData({
+          title: "",
+          content: "",
+          image: "",
+          tags: "",
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
@@ -22,6 +55,8 @@ const AddPost = () => {
             type="text"
             placeholder="Enter post title"
             name="title"
+            value={formData.title}
+            onChange={handleChange}
           />
         </Form.Group>
 
@@ -31,6 +66,8 @@ const AddPost = () => {
             type="text"
             placeholder="Enter post content"
             name="content"
+            value={formData.content}
+            onChange={handleChange}
           />
         </Form.Group>
 
@@ -40,6 +77,8 @@ const AddPost = () => {
             type="text"
             placeholder="Enter post image URL"
             name="image"
+            value={formData.image}
+            onChange={handleChange}
           />
         </Form.Group>
 
@@ -49,6 +88,8 @@ const AddPost = () => {
             type="text"
             placeholder="Enter comma separated tags"
             name="tags"
+            value={formData.tags}
+            onChange={handleChange}
           />
         </Form.Group>
 
